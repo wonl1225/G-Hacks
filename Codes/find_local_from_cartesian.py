61e79de9babb4ccbdec6807cd73f431866dd713d
finnwarwick <finnwarwick@gmail.com>
2026-02-07 23:10:00 -0700

    Create find_local_from_cartesian.py

---
+++ Codes/find_local_from_cartesian.py
@@ -0,0 +1,23 @@
+print('hello')
+
+while True:
+
+
+    num1x = float(input('num 1 x'))
+    
+
+    numoutx = num1x-(-1641893.07187132)
+    
+    num1y = float(input('num 1 y'))
+    
+
+    numouty = num1y-(-3664917.00956553)
+    
+    num1z = float(input('num 1z '))
+    
+
+    numoutz = num1z-(4939937.44701297)
+
+    print(numoutx) 
+    print(numouty)
+    print(numoutz)
